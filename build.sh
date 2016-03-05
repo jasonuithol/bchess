d648b4c00a8242bfb02856b201b4d0e438fb8605
easyCoder <jasonuithol@gmail.com>
2016-03-05 12:51:43 +1100

    Now using ALL THE OPTIMISATIONS

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #gcc -std=c99 -Wextra -Ofast -march=native -fomit-frame-pointer -flto -fprofile-generate -DPROFILING_BUILD bchess.c
 #./run.sh
 #gcc -std=c99 -Wextra -Ofast -march=native -fomit-frame-pointer -flto -fprofile-use bchess.c
-gcc -std=c99 -Wextra -march=native bitboard.c
+gcc -std=c99 -Wextra -march=native -Ofast -fomit-frame-pointer -flto -fuse-linker-plugin -fwhole-program bbchess.c
 
