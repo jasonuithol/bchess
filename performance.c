386d25ab4956ccfd174a2193e3ef02225ab7afaf
easyCoder <jasonuithol@gmail.com>
2016-03-05 18:19:56 +1100

    Added new perf test for getBestMove

--- performance.c
+++ performance.c
@@ -42,6 +42,16 @@ double perftest_evaluateMobility_Initial() {
 	return difftime(finishTime, startTime);
 } 
 
+double perftest_getBestMove_Initial() {
+	analysisMove bestMove;
+	board b;	
+	initBoard(&b);
+	time_t startTime = time(NULL);
+	getBestMove(&bestMove, &b, 0, 2, 0);
+	time_t finishTime = time(NULL);
+	return difftime(finishTime, startTime);
+}
+
 
 
 void runPerformanceSuite() {
@@ -49,6 +59,7 @@ void runPerformanceSuite() {
 	double a2 = perftest_evaluateMaterial();
 	double a3 = perftest_evaluateMobility_Empty();
 	double a4 = perftest_evaluateMobility_Initial();
+	double a5 = perftest_getBestMove_Initial();
 	
-	printf("Performance results: %f %f %f %f\n", a1, a2, a3, a4);
+	print("Performance results: %f %f %f %f %f\n", a1, a2, a3, a4, a5);
 }
