35c82099f330e1a2253ff28a049d527205fcaea6
easyCoder <jasonuithol@gmail.com>
2016-03-06 13:02:29 +1100

    Tried some compiler setting changes

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 #./run.sh
 #gcc -std=c99 -Wextra -Ofast -march=native -fomit-frame-pointer -flto -fprofile-use bchess.c
 gcc -std=c99 -Wextra -march=native -Ofast -fomit-frame-pointer -flto -fuse-linker-plugin -fwhole-program bbchess.c
+#clang -std=c99 -Wextra -march=native -O3 -fomit-frame-pointer -flto bbchess.c
+#gcc -std=c99 -Wextra -march=native -O2 bbchess.c
 
